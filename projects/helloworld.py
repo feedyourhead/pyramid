279711e295db7688456cd02f1986b57572b866f1
daniel.w <daniel.w@webinterpret.com>
2015-11-18 23:22:24 +0100

    added a kitten!

--- projects/helloworld.py
+++ projects/helloworld.py
@@ -6,10 +6,15 @@ from pyramid.response import Response
 def hello_world(request):
     return Response('Hello %(name)s!' % request.matchdict)
 
+def show_kitten(request):
+    return Response('<body><img alt="" src="http://res.freestockphotos.biz/pictures/9/9343-a-cute-orange-kitten-isolated-on-a-white-background-pv.jpg"></body>')
+
 if __name__ == '__main__':
     config = Configurator()
     config.add_route('hello', '/hello/{name}')
     config.add_view(hello_world, route_name='hello')
+    config.add_route('kitten', '/')
+    config.add_view(show_kitten, route_name='kitten')
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
